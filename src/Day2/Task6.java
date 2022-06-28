91faf94de6731cb710763937908c89c6b828c520
NadiaGura <gura.nadezda@gmail.com>
2022-06-28 17:00:23 +0300

    commit

--- src/Day2/Task6.java
+++ src/Day2/Task6.java
@@ -25,29 +25,26 @@ public class Task6 {
 
     public static void main(String[] args) {
 
-        int n1;  //input variables
-        int n2;
+        int a;  //input variables
+        int b;
 
         //SCANNER
 
         Scanner sc = new Scanner(System.in); //Scanner class object to be used for user input
 
         System.out.println("Please enter first number:"); //asking user for first number
-        n1 = sc.nextInt();
-        System.out.println("Please enter second number:"); //asking user for first number
-        n2 = sc.nextInt();
+        a = sc.nextInt();
+        System.out.println("Please enter second number:"); //asking user for second number
+        b = sc.nextInt();
 
 
         //METHOD TO CALCULATE IF NUMBER IS ODD OR EVEN
 
-//        if (n1 > 100 & n2 > 100) {
-//            System.out.println(true);
-        } else {
-            if (n1 == n2 || n1 > 0 || n2 > 0) {
-                System.out.println(true);
+        if (a == b || a > 0 && b > 0 || a > 100 && b > 100) {
+            System.out.println(true);
             } else {
                 System.out.println(false);
             }
         }
     }
-//}
+
