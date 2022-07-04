3802d9d8c87d99409259c232cd08acac3764447b
NadiaGura <gura.nadezda@gmail.com>
2022-07-04 22:29:20 +0300

    init

--- src/Day4/Collections.java
+++ src/Day4/Collections.java
@@ -18,6 +18,60 @@ public class Collections {
 
         System.out.println(colors);
 
+        //Task2. Iterate through all elements in an array List.
+        for(String elements: colors){
+            System.out.println(colors);
+        }
+
+        //Task3. Insert an element into the array List at the first position.
+        colors.add(0,"navy");
+
+        //Task4. Retrieve an element(at specific index) from an AArray list.
+        colors.get(2);
+
+        //Task5. Update specific array element by given element.
+        colors.set(0,"gold");
+
+        //Task6. Remove third element from an Array List.
+        colors.remove(2);
+
+        //Task7. Search an element in an Array List.
+        if(colors.contains("gold")){
+            System.out.println("gold");
+        }else{
+            System.out.println("Element not found.");
+        }
+
+        //Task8. Sort a given Array List.
+        // CORRECT Collections.sort(colors);
+
+        //Task9. Copy one Array List to another.
+
+
+        //Task10. Shuffle elements in an Array List.
+
+        //Task11. Reverse elements in an Array List.
+
+        //Task12. Extract a portion of an Array List.
+
+        //Task13. Compare two Array Lists.
+
+        //Task14. Swap two elements in an Array List.
+
+        //Task15. Join two Array Lists.
+
+        //Task16. Clone one Array list to another.
+
+        //Task17. Empty an Array List.
+
+        //Task18. Test if an Array List is empty or not.
+
+        //Task19. Trim the capacity of an Array List the current list size.
+
+        //Task20. Increase the size of an Array List.
+
+        //Task21. Replace the second element of an Array List with the specified element.
+
         //Task22.Print all the elements of an ArrayList using the position of the element.
         for(int i = 0; i< colors.size();i++) //iterate through the ArrayList to print each element in the consequence of its index
             System.out.println(colors.get(i));
